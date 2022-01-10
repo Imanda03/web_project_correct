6038d4f3444e5823e55be038388adbea42c442b8
Imanda03 <asis03ktm@gmail.com>
2022-01-10 19:25:09 +0545

    New updated

---
+++ php file/connection.php
@@ -0,0 +1,11 @@
+<?php
+    $host="localhost";
+    $user="root";
+    $password="";
+    $db_name="login_db";
+
+    $conn = new mysqli($host,$user,$password,$db_name);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    ?>
